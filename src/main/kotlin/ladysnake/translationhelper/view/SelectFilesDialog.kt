bd94c3e0c25b08f0c4cc2030166370e14922bf6f
Pyrofab <redstoneinfire@gmail.com>
2018-12-23 22:59:03 +0100

    Add an "open all" checkbox in file selection

--- src/main/kotlin/ladysnake/translationhelper/view/SelectFilesDialog.kt
+++ src/main/kotlin/ladysnake/translationhelper/view/SelectFilesDialog.kt
@@ -18,18 +18,29 @@ import java.io.File
 class SelectFilesDialog(files: List<File>) : Dialog<SourcesMap>() {
 
     init {
-        val list = FXCollections
-            .observableList(files.map { f -> ExtendedFile(f, true, false) })
-        for (f in list.reversed()) {    // Lazy way of having .json be opened instead of .lang by default
-            if (list.any { it != f && it.file.nameWithoutExtension == f.file.nameWithoutExtension && it.isToOpen.get() }) {
-                f.isToOpen.set(false)
-            }
+        val list = FXCollections.observableArrayList<ExtendedFile>()
+        files.mapTo(list) { f ->
+            ExtendedFile(f, list.none { it.file.nameWithoutExtension == f.nameWithoutExtension && it.isToOpen.get() }, false)
         }
         this.dialogPane.content = tableview(list) {
             isEditable = true
             column("Open", CheckBoxTableCell::class) {
                 setCellFactory { CheckBoxTableCell { i -> list[i].isToOpen } }
                 isEditable = true
+                prefWidth = 60.0
+                graphic = checkbox {
+                    selectedProperty().addListener { _, _, openAll ->
+                        for (f in list) {    // Lazy way of having .json be opened instead of .lang by default
+                            if (list.any { it != f && it.file.nameWithoutExtension == f.file.nameWithoutExtension && it.isToOpen.get() }) {
+                                f.isToOpen.set(false)
+                            } else {
+                                f.isToOpen.set(openAll)
+                            }
+                        }
+                    }
+                    isSelected = false
+                    tooltip("Open none/all of the files")
+                }
             }
             column("Lock", CheckBoxTableCell::class) {
                 setCellFactory { CheckBoxTableCell { i -> list[i].isToLock } }
